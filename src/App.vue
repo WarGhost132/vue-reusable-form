4ee406f3e432b2ad8d73aba8188ac799fb4999f8
WarGhost132 <wawghostick@gmail.com>
2025-03-24 13:09:45 +0300

    setup sass

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,21 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div class="box">Привет, Sass!</div>
+
   <RouterView />
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.box {
+  padding: 20px;
+  background: $primary-color;
+  color: white;
+  font-size: 20px;
+  border-radius: 5px;
+
+  &:hover {
+    background: $primary-color-hover;
+  }
+}
+</style>
